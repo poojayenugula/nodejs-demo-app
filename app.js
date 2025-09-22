217f3ac3c23b14b9770d2eafb3187abe930a423d
root <root@ip-172-31-24-135.ec2.internal>
2025-09-22 10:24:39 +0000

    Add CI/CD GitHub Actions workflow

---
+++ app.js
@@ -0,0 +1,12 @@
+cat <<EOF > app.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello from CI/CD Pipeline!'));
+
+app.listen(PORT, () => {
+  console.log(\`Server running on port \${PORT}\`);
+});
+EOF
+
